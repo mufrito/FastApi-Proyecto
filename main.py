09dcf1646b6eb2dc8229400895c05440c8ea6eb2
Samuel Díaz <sammu.diazs@gmail.com>
2026-05-09 00:35:29 +0000

    css

--- main.py
+++ main.py
@@ -2,29 +2,38 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+BASE_DIR = Path(__file__).resolve().parent
+
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "static"),
+    name="static"
+)
+
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def inicio(request: Request):
     return templates.TemplateResponse(
-        request=request, name="index.html"
+        request=request,
+        name="index.html"
     )
 
 @app.get("/personas", response_class=HTMLResponse)
-async def inicio(request: Request):
+async def personas(request: Request):
     return templates.TemplateResponse(
-        request=request, name="personas.html"
+        request=request,
+        name="personas.html"
     )
 
 @app.get("/Tartaro", response_class=HTMLResponse)
-async def inicio(request: Request):
+async def tartaro(request: Request):
     return templates.TemplateResponse(
-        request=request, name="Tartaro.html"
+        request=request,
+        name="tartaro.html"
     )
-
-
